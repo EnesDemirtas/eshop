9ac5b82175268b247df8ce13942278d191117f8b
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-08-22 16:19:21 +0300

    create category table migration

--- EshopWeb/Data/ApplicationDbContext.cs
+++ EshopWeb/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using EshopWeb.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EshopWeb.Data;
@@ -6,4 +7,6 @@ public class ApplicationDbContext : DbContext {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
         
     }
+
+    public DbSet<Category> Categories {get; set;}
 }
