7d88aa32fff144e24427f1f1cf82b7face6dfca4
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-08-22 15:12:17 +0300

    create category model

---
+++ EshopWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EshopWeb.Models;
+
+public class Category {
+    [Key]
+    public int Id {get; set;}
+    [Required]
+    public string Name {get; set;}
+    public int DisplayOrder {get; set;}
+}
