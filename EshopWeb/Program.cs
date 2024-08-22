545d413821bee175daa402705e86e700ff10d350
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-08-22 16:05:03 +0300

    create ApplicationDbContext

--- EshopWeb/Program.cs
+++ EshopWeb/Program.cs
@@ -1,7 +1,13 @@
+using EshopWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
