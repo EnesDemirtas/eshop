21695b7ff3c2b21c896d9452847f7470794cc298
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-08-23 00:51:13 +0300

    add edit and delete for category

--- EshopWeb/Controllers/CategoryController.cs
+++ EshopWeb/Controllers/CategoryController.cs
@@ -28,4 +28,37 @@ public class CategoryController : Controller {
         }
         return View();
     }
+
+    public IActionResult Edit(int? id) {
+        if (id == null || id == 0) return NotFound();
+        Category? category = _context.Categories.FirstOrDefault(u => u.Id == id);
+        if (category == null) return NotFound();
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj) {
+        if (ModelState.IsValid) {
+            _context.Categories.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id) {
+        if (id == null || id == 0) return NotFound();
+        Category? category = _context.Categories.FirstOrDefault(u => u.Id == id);
+        if (category == null) return NotFound();
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePOST(int? id) {
+        Category? obj = _context.Categories.FirstOrDefault(u => u.Id == id);
+        if (obj == null) return NotFound();
+        _context.Categories.Remove(obj);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
