3fb406dbe3275e1e63ee2cbf132535f6efc24439
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-08-26 00:31:28 +0300

    implement product entity and all crud operations

---
+++ EshopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+using Eshop.Models;
+using Eshop.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EshopWeb.Controllers;
+
+[Area("Admin")]
+public class ProductController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public ProductController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult Index()
+    {
+        List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+        return View(objProductList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Product.Add(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product has been created successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+        Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+        if (product == null) return NotFound();
+        return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Product obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Product.Update(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product has been updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+        Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+        if (product == null) return NotFound();
+        return View(product);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePOST(int? id)
+    {
+        Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+        if (obj == null) return NotFound();
+        _unitOfWork.Product.Remove(obj);
+        _unitOfWork.Save();
+        TempData["success"] = "Product has been deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
