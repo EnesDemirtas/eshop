eb12453d04fa23de21934e98e1f28c14b10d53f3
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-09-02 04:45:11 +0300

    let admin manage users' roles

---
+++ Eshop.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Models.ViewModels;
+
+public class RoleManagementVM
+{
+    public ApplicationUser ApplicationUser { get; set; }
+    public IEnumerable<SelectListItem> RoleList { get; set; }
+    public IEnumerable<SelectListItem> CompanyList { get; set; }
+}
--- EshopWeb/Areas/Admin/Controllers/UserController.cs
+++ EshopWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,11 +1,13 @@
 using Eshop.DataAccess.Data;
 using Eshop.DataAccess.Repository.IRepository;
 using Eshop.Models;
+using Eshop.Models.ViewModels;
 using Eshop.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Stripe;
 
 namespace EshopWeb.Areas.Admin.Controllers;
 
@@ -14,18 +16,70 @@ namespace EshopWeb.Areas.Admin.Controllers;
 public class UserController : Controller
 {
 	private readonly ApplicationDbContext _context;
-	public UserController(ApplicationDbContext context)
+	private readonly UserManager<IdentityUser> _userManager;
+	public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
 	{
 		_context = context;
+		_userManager = userManager;
 	}
 	public IActionResult Index()
 	{
 		return View();
 	}
 
-	#region API CALLS
+	public IActionResult RoleManagement(string userId)
+	{
+		string RoleID = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userId).RoleId;
 
-	[HttpGet]
+		RoleManagementVM RoleVM = new()
+		{
+			ApplicationUser = _context.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+			RoleList = _context.Roles.Select(i => new SelectListItem
+			{
+				Text = i.Name,
+				Value = i.Name
+			}),
+			CompanyList = _context.Companies.Select(i => new SelectListItem
+			{
+				Text = i.Name,
+				Value = i.Id.ToString()
+			})
+		};
+
+		RoleVM.ApplicationUser.Role = _context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+		return View(RoleVM);
+	}
+
+	[HttpPost]
+    public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+    {
+        string RoleID = _context.UserRoles.FirstOrDefault(ur => ur.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+		string oldRole = _context.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+		if (!(roleManagementVM.ApplicationUser.Role == oldRole))
+		{
+			ApplicationUser applicationUser = _context.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+			if (roleManagementVM.ApplicationUser.Role == StaticDetails.Role_Company)
+			{
+				applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+			}
+			if (oldRole == StaticDetails.Role_Company)
+			{
+				applicationUser.CompanyId = null;
+			}
+			_context.SaveChanges();
+
+			_userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+			_userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    #region API CALLS
+
+    [HttpGet]
 	public IActionResult GetAll()
 	{
 		List<ApplicationUser> objUserList = _context.ApplicationUsers.Include(u => u.Company).ToList();
